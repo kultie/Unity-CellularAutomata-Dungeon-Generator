42990e6cd0f3b8492fcce7f7d9bbabed905c930a
longnt <longnt1@kaopiz.com>
2019-08-19 16:03:24 +0700

    Initial commit

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Kultie.ProcedualDungeon;
+public class Controller : MonoBehaviour {
+    Dungeon dungeon;
+    public GameObject cellsContainer;
+    public RectTransform mapCellPrefab;
+    public float cellSize;
+
+
+    bool[,] map;
+    private int dungeonWidth = 75;
+    private int dungeonHeight = 75;
+
+    Dictionary<string, RectTransform> cacheObj = new Dictionary<string, RectTransform>();
+	// Use this for initialization
+	void CreateMap () {
+        dungeon = new Dungeon(dungeonWidth, dungeonHeight);
+        map = dungeon.GetDungeonGrid();
+        for (int i = 0; i < 6; i++)
+        {
+            map = dungeon.SimulationStep(map);
+        }
+        DrawMap();
+	}
+
+	private void Update()
+	{
+        if(Input.GetKeyDown(KeyCode.Space)){
+            CreateMap();
+        }
+        else if(Input.GetKeyDown(KeyCode.Z)){
+            PlaceTresure();
+        }
+	}
+
+
+
+	void DrawMap(){
+        for (int i = 0; i < map.GetLength(0); i++)
+        {
+            for (int j = 0; j < map.GetLength(1); j++)
+            {
+                string key = i.ToString() + "-" + j.ToString();
+                RectTransform cell = null;
+                if(!cacheObj.TryGetValue(key, out cell)){
+                    cell = Instantiate(mapCellPrefab, cellsContainer.transform);
+                    cacheObj[key] = cell;
+                }
+                DrawMapCell(cell, new Vector2(i, j), map[i, j]);
+            }
+        }
+    }
+	
+    void DrawMapCell(RectTransform mapCell, Vector2 position, bool value){
+        mapCell.localScale = Vector3.one;
+        mapCell.sizeDelta = Vector2.one * cellSize;
+        mapCell.transform.localPosition = position * cellSize - new Vector2(dungeonWidth * cellSize / 2,dungeonHeight * cellSize / 2);
+        mapCell.GetComponent<Image>().color = value ? Color.white : Color.black;
+    }
+
+    void PlaceTresure(){
+        for (int i = 0; i < map.GetLength(0); i++)
+        {
+            for (int j = 0; j < map.GetLength(1); j++)
+            {
+                if(!map[i,j]){
+                    int nbs = dungeon.CountAliveNeightbours(map, i, j);
+                    if(nbs >= 6){
+                        if(Random.Range(0f,1f) >= 0.75f){
+                            string key = i.ToString() + "-" + j.ToString();
+                            cacheObj[key].GetComponent<Image>().color = Color.yellow;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Dungeon.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Kultie.ProcedualDungeon
+{
+    public class Dungeon
+    {
+        private bool[,] dungeonGrid;
+        private float chanceToStartAlive = 0.45f;
+
+        private int overpopLimit = 8;
+
+        private int starvationLimit = 5;
+
+        private int birthLimit = 3;
+
+        public Dungeon(int width, int height)
+        {
+            dungeonGrid = InitialiseMap(width, height);
+        }
+
+        bool[,] InitialiseMap(int width, int height)
+        {
+            bool[,] dungeon = new bool[width, height];
+            for (int i = 0; i < dungeon.GetLength(0); i++)
+            {
+                for (int j = 0; j < dungeon.GetLength(1); j++)
+                {
+                    float randomValue = Random.Range(0f, 1f);
+                    dungeon[i, j] = randomValue < chanceToStartAlive;
+                }
+            }
+            return dungeon;
+        }
+
+        public void PrintMapValue()
+        {
+            for (int i = 0; i < dungeonGrid.GetLength(0); i++)
+            {
+                for (int j = 0; j < dungeonGrid.GetLength(1); j++)
+                {
+                    Debug.Log("x: " + i + " y: " + j + " value: " + dungeonGrid[i, j]);
+                }
+            }
+        }
+
+        public bool[,] GetDungeonGrid()
+        {
+            return dungeonGrid;
+        }
+
+        public bool[,] SimulationStep(bool[,] oldDungeon)
+        {
+            bool[,] newDungeon = new bool[oldDungeon.GetLength(0), oldDungeon.GetLength(1)];
+            for (int i = 0; i < oldDungeon.GetLength(0); i++)
+            {
+                for (int j = 0; j < oldDungeon.GetLength(1); j++)
+                {
+                    int neightboursCount = CountAliveNeightbours(oldDungeon, i, j);
+                    if(oldDungeon[i,j]){
+                        newDungeon[i, j] = neightboursCount >= overpopLimit && neightboursCount <= starvationLimit;
+                    }
+                    else{
+                        newDungeon[i, j] = neightboursCount <= birthLimit;
+                    }
+                }
+            }
+            return newDungeon;
+        }
+
+        public int CountAliveNeightbours(bool[,] dungeon, int x, int y)
+        {
+            int count = 0;
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    int neightbour_x = x + i;
+                    int neightbour_y = y + j;
+                    if (i == 0 && j == 0)
+                    {
+
+                    }
+                    else if (neightbour_x < 0 || neightbour_y < 0 || neightbour_x >= dungeon.GetLength(0) || neightbour_y >= dungeon.GetLength(1))
+                    {
+                        
+                    }
+                    else if(dungeon[neightbour_x,neightbour_y]){
+                        count = count + 1;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
+
