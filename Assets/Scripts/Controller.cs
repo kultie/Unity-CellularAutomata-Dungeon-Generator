6429d2426bd9323cdbe848f900309f40b7f9efe8
longnt <longnt1@kaopiz.com>
2019-08-22 15:56:54 +0700

    Add 4-bit bitmask auto tile to automatically fill tile value

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -6,7 +6,8 @@ using Kultie.ProcedualDungeon;
 using System.Threading;
 using System;
 
-public class Controller : MonoBehaviour {
+public class Controller : MonoBehaviour
+{
     Dungeon dungeon;
     public GameObject cellsContainer;
     public SpriteRenderer mapCellPrefab;
@@ -14,185 +15,80 @@ public class Controller : MonoBehaviour {
     public int dungeonWidth = 200;
     public int dungeonHeight = 200;
     DungeonCell[,] map;
-
+    public Sprite[] sprites;
     int fillSpace;
 
-    Dictionary<string, SpriteRenderer> cacheImg = new Dictionary<string, SpriteRenderer>();
+    private void Start()
+    {
+        ObjectPool.CreatePool(mapCellPrefab, 1000);
+    }
     // Use this for initialization
-    void CreateMap () {
-       
+    void CreateMap()
+    {
+
         fillSpace = 0;
-        StartCoroutine(GenerateMap(()=>{
+        StartCoroutine(GenerateMap(() =>
+        {
             map = dungeon.GetDungeonGrid();
+            //DrawMap();
             DrawMap();
-            FloodFill(map, dungeonWidth / 2, dungeonHeight / 2, Color.white);
-            ClearAllLeftOver();
-            DrawMap();
-            AddTerrain();
-            float filledRate = fillSpace * 1f / (dungeonWidth * dungeonHeight);
-            if (filledRate < 0.4f)
-            {
-                CreateMap();
-            }
         }));
     }
 
-    IEnumerator GenerateMap(Action callback){
+    IEnumerator GenerateMap(Action callback)
+    {
         bool done = false;
         Thread thread = new Thread(() =>
         {
             dungeon = new Dungeon(dungeonWidth, dungeonHeight);
-            done = true;
+            while (!done)
+            {
+                done = dungeon.CreateMap();
+            }
         });
         thread.Start();
-        while(!done){
+        while (!done)
+        {
             yield return null;
         }
-        Debug.Log("Finish generate Map");
         callback();
     }
 
-	private void Update()
-	{
-        if(Input.GetKeyDown(KeyCode.Space)){
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             CreateMap();
         }
-        else if(Input.GetKeyDown(KeyCode.Z)){
-            PlaceTresure();
-        }
-        else if (Input.GetKeyDown(KeyCode.X))
-        {
-            PlaceRock();
-        }
-	}
+    }
 
-	void DrawMap(){
+    void DrawMap()
+    {
+        ObjectPool.RecycleAll(mapCellPrefab);
         for (int i = 0; i < dungeonWidth; i++)
         {
             for (int j = 0; j < dungeonHeight; j++)
             {
-                string key = i.ToString() + "-" + j.ToString();
-                SpriteRenderer cell = null;
-                if(!cacheImg.TryGetValue(key, out cell)){
-                    cell = Instantiate(mapCellPrefab, cellsContainer.transform);
-                    cacheImg[key] = cell;
-                }
-                DrawMapCell(cell, new Vector2(i, j), map[i, j].cellType == DungeonCellType.PATH);
+                SpriteRenderer cell = ObjectPool.Spawn(mapCellPrefab, cellsContainer.transform);
+                DrawMapCell(cell, new Vector2(i, j), map[i, j]);
             }
         }
     }
-	
-    void DrawMapCell(SpriteRenderer mapCell, Vector2 position, bool value){
+
+    void DrawMapCell(SpriteRenderer mapCell, Vector2 position, DungeonCell data)
+    {
         Transform cellTransform = mapCell.transform;
         cellTransform.localScale = Vector3.one;
-        cellTransform.localPosition = position - new Vector2(dungeonWidth/ 2,dungeonHeight/ 2);
-        mapCell.color = value ? Color.blue : Color.grey;
-    }
-
-    void PlaceTresure(){
-        for (int i = 0; i < map.GetLength(0); i++)
+        cellTransform.localPosition = position - new Vector2(dungeonWidth / 2, dungeonHeight / 2);
+        Color mapCellColor = Color.white;
+        if (data.cellType == DungeonCellType.PATH)
         {
-            for (int j = 0; j < map.GetLength(1); j++)
-            {
-                if(i == 0 || j == 0 || i == map.GetLength(0) - 1 || j == map.GetLength(1) - 1){
-                    if (map[i, j].cellType == DungeonCellType.PATH)
-                    {
-                        int nbs = dungeon.CountAliveNeightbours(map, i, j);
-                        if (nbs >= 3)
-                        {
-                            string key = i.ToString() + "-" + j.ToString();
-                            cacheImg[key].color = Color.yellow;
-                        }
-                    }
-                }
-            }
+            mapCellColor = new Color(0, 1 * data.pathValue, 0, 1);
         }
-    }
-
-    void PlaceRock()
-    {
-        for (int i = 0; i < map.GetLength(0); i++)
+        else if (data.cellType == DungeonCellType.WALL)
         {
-            for (int j = 0; j < map.GetLength(1); j++)
-            {
-                if (map[i, j].cellType == DungeonCellType.WALL)
-                {
-                    int nbs = dungeon.CountAliveNeightbours(map, i, j);
-                    if (nbs >= 1 && nbs <= 8)
-                    {
-                        string key = i.ToString() + "-" + j.ToString();
-                        cacheImg[key].color = Color.red;
-                    }
-                }
-            }
+            mapCell.sprite = sprites[data.spriteValue];
         }
-    }
-
-    void AddTerrain()
-    {
-        Vector2 offSet = Vector2.one * UnityEngine.Random.Range(-100f, 100f);
-        for (int i = 0; i < dungeonWidth; i++)
-        {
-            for (int j = 0; j < dungeonHeight; j++)
-            {
-                string key = i + "-" + j;
-                if (cacheImg[key].color == Color.white || cacheImg[key].color == Color.blue)
-                {
-                    float data = GenerateTerrain(i, j, dungeonWidth, dungeonHeight, 2, offSet);
-                    cacheImg[key].color = new Color(0.5f, 1 * data, 0.5f, 1f);
-                }
-                if (cacheImg[key].color == Color.grey)
-                {
-                    float data = GenerateTerrain(i, j, dungeonWidth, dungeonHeight, 2, offSet);
-                    cacheImg[key].color = new Color(1, 1 * data, 1, 1f);
-                }
-            }
-        }
-    }
-
-    float GenerateTerrain(int x, int y, int mapWidth, int mapHeight, float scale, Vector2 offSet)
-    {
-        float xCoord = (float)x / mapWidth * scale;
-        float yCoord = (float)y / mapHeight * scale;
-        return Mathf.PerlinNoise(xCoord + offSet.x, yCoord + offSet.y);
-    }
-
-    void FloodFillUtil(DungeonCell[,] _map, int x, int y, Color prevC, Color newC)
-    {      
-        if (x < 0 || x >= _map.GetLength(0) || y < 0 || y >= _map.GetLength(1))
-            return;
-
-        string key = x.ToString() + "-" + y.ToString();
-        var currentNode = cacheImg[key];
-
-        if (currentNode.color != prevC)
-            return;
-
-        currentNode.color = newC;
-        fillSpace++;
-        FloodFillUtil(_map, x + 1, y, prevC, newC);
-        FloodFillUtil(_map, x - 1, y, prevC, newC);
-        FloodFillUtil(_map, x, y + 1, prevC, newC);
-        FloodFillUtil(_map, x, y - 1, prevC, newC);
-    }
-
-    void FloodFill(DungeonCell[,] _map, int x, int y, Color newC)
-    {
-        string key = x.ToString() + "-" + y.ToString();
-        var prevC = cacheImg[key].color;
-        FloodFillUtil(_map, x, y, prevC, newC);
-    }
-
-    void ClearAllLeftOver(){
-        for (int i = 0; i < dungeonWidth; i++)
-        {
-            for (int j = 0; j < dungeonHeight; j++)
-            {
-                string key = i + "-" + j;
-                if(map[i,j].cellType == DungeonCellType.PATH && cacheImg[key].color == Color.blue){
-                    map[i, j].SetCellType(DungeonCellType.WALL);
-                }
-            }
-        }
+        mapCell.color = mapCellColor;
     }
 }
--- Assets/Scripts/Dungeon.cs
+++ Assets/Scripts/Dungeon.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 namespace Kultie.ProcedualDungeon
 {
     public enum DungeonCellType { PATH, WALL };
+    public enum DungeonCellFillType { FILLED, NON };
     public class Dungeon
     {
         private DungeonCell[,] dungeonGrid;
@@ -14,27 +15,62 @@ namespace Kultie.ProcedualDungeon
 
         private int birthLimit = 4;
 
+        int fillSpace = 0;
+
+        int mapWidth;
+        int mapHeight;
+
+        Random rnd;
+
         public Dungeon(int width, int height)
         {
-            dungeonGrid = InitialiseMap(width, height);
+            mapWidth = width;
+            mapHeight = height;
+            rnd = new Random();
+
+        }
+        public bool CreateMap()
+        {
+            fillSpace = 0;
+            dungeonGrid = InitialiseMap(mapWidth, mapHeight);
             for (int i = 0; i < 12; i++)
             {
                 dungeonGrid = SimulationStep(dungeonGrid);
             }
+            FloodFill(dungeonGrid, mapWidth / 2, mapHeight / 2, DungeonCellFillType.FILLED);
+            float filledRate = fillSpace * 1f / (mapWidth * mapHeight);
+            if (filledRate < 0.4f)
+            {
+                return false;
+            }
+            ClearAllLeftOver();
+            AddTerrain();
+            return true;
         }
-        public void CreateMap(){
-            
+
+        void ClearAllLeftOver()
+        {
+            for (int i = 0; i < mapWidth; i++)
+            {
+                for (int j = 0; j < mapHeight; j++)
+                {
+                    string key = i + "-" + j;
+                    if (dungeonGrid[i, j].cellType == DungeonCellType.PATH && dungeonGrid[i, j].fillType == DungeonCellFillType.NON)
+                    {
+                        dungeonGrid[i, j].SetCellType(DungeonCellType.WALL);
+                    }
+                }
+            }
         }
 
         DungeonCell[,] InitialiseMap(int width, int height)
         {
             DungeonCell[,] dungeon = new DungeonCell[width, height];
-            Random rnd = new Random();
             for (int i = 0; i < dungeon.GetLength(0); i++)
             {
                 for (int j = 0; j < dungeon.GetLength(1); j++)
                 {
-                    float randomValue = rnd.Next(0,100);
+                    float randomValue = rnd.Next(0, 100);
                     dungeon[i, j] = new DungeonCell(randomValue < chanceToStartAlive ? DungeonCellType.PATH : DungeonCellType.WALL);
 
                 }
@@ -55,10 +91,12 @@ namespace Kultie.ProcedualDungeon
                 for (int j = 0; j < oldDungeon.GetLength(1); j++)
                 {
                     int neightboursCount = CountAliveNeightbours(oldDungeon, i, j);
-                    if(oldDungeon[i,j].cellType == DungeonCellType.PATH){
+                    if (oldDungeon[i, j].cellType == DungeonCellType.PATH)
+                    {
                         newDungeon[i, j] = new DungeonCell(neightboursCount >= overpopLimit && neightboursCount <= starvationLimit ? DungeonCellType.PATH : DungeonCellType.WALL);
                     }
-                    else if (oldDungeon[i, j].cellType == DungeonCellType.WALL){
+                    else if (oldDungeon[i, j].cellType == DungeonCellType.WALL)
+                    {
                         newDungeon[i, j] = new DungeonCell(neightboursCount <= birthLimit ? DungeonCellType.PATH : DungeonCellType.WALL);
                     }
                 }
@@ -82,31 +120,167 @@ namespace Kultie.ProcedualDungeon
                     }
                     else if (neightbour_x < 0 || neightbour_y < 0 || neightbour_x >= dungeon.GetLength(0) || neightbour_y >= dungeon.GetLength(1))
                     {
-                        
+
                     }
-                    else if(dungeon[neightbour_x,neightbour_y].cellType == DungeonCellType.PATH){
+                    else if (dungeon[neightbour_x, neightbour_y].cellType == DungeonCellType.PATH)
+                    {
                         count = count + 1;
                     }
                 }
             }
             return count;
         }
+
+        void FloodFillUtil(DungeonCell[,] _map, int x, int y, DungeonCellFillType prevC, DungeonCellFillType newC)
+        {
+            if (x < 0 || x >= _map.GetLength(0) || y < 0 || y >= _map.GetLength(1))
+                return;
+
+            var currentNode = _map[x, y];
+
+            if (currentNode.fillType != prevC || currentNode.cellType == DungeonCellType.WALL)
+                return;
+
+            currentNode.SetFillType(newC);
+            fillSpace++;
+            FloodFillUtil(_map, x + 1, y, prevC, newC);
+            FloodFillUtil(_map, x - 1, y, prevC, newC);
+            FloodFillUtil(_map, x, y + 1, prevC, newC);
+            FloodFillUtil(_map, x, y - 1, prevC, newC);
+        }
+
+        void FloodFill(DungeonCell[,] _map, int x, int y, DungeonCellFillType newC)
+        {
+            var prevC = _map[x, y].fillType;
+            FloodFillUtil(_map, x, y, prevC, newC);
+        }
+
+        void AddTerrain()
+        {
+            int offSet = rnd.Next(-100, 100);
+            for (int i = 0; i < mapWidth; i++)
+            {
+                for (int j = 0; j < mapHeight; j++)
+                {
+                    if (dungeonGrid[i, j].cellType == DungeonCellType.PATH)
+                    {
+                        float data = GenerateTerrain(i, j, mapWidth, mapHeight, 2, offSet);
+                        dungeonGrid[i, j].pathValue = data;
+                    }
+                    if (dungeonGrid[i, j].cellType == DungeonCellType.WALL)
+                    {
+                        CalculateCellSpriteValue(i, j, DungeonCellType.WALL);
+                        float data = GenerateTerrain(i, j, mapWidth, mapHeight, 2, offSet);
+                        dungeonGrid[i, j].wallValue = data;
+                    }
+                }
+            }
+        }
+
+        float GenerateTerrain(int x, int y, int width, int height, float scale, int offSet)
+        {
+            float xCoord = (float)x / width * scale;
+            float yCoord = (float)y / height * scale;
+            float value = UnityEngine.Mathf.PerlinNoise(xCoord + offSet, yCoord + offSet);
+            return value;
+            //if (value <= 0.25) return 0;
+            //else if (value <= 0.75) return 0.5f;
+            //return 1;
+        }
+
+        public void CalculateCellSpriteValue(int x, int y, DungeonCellType compareCellType)
+        {
+            bool upCell;
+            bool leftCell;
+            bool rightCell;
+            bool downCell;
+
+            if (y >= mapHeight - 1)
+            {
+                upCell = false;
+            }
+            else
+            {
+                upCell = dungeonGrid[x, y + 1].cellType == compareCellType;
+            }
+
+            if (x <= 0)
+            {
+                leftCell = false;
+            }
+            else
+            {
+                leftCell = dungeonGrid[x - 1, y].cellType == compareCellType;
+            }
+
+            if (x >= mapWidth - 1)
+            {
+                rightCell = false;
+            }
+            else
+            {
+                rightCell = dungeonGrid[x + 1, y].cellType == compareCellType;
+            }
+
+            if (y <= 0)
+            {
+                downCell = false;
+            }
+            else
+            {
+                downCell = dungeonGrid[x, y - 1].cellType == compareCellType;
+            }
+
+            int value = 1 * ConvertBoolToInt(upCell) + 2 * ConvertBoolToInt(leftCell) + 4 * ConvertBoolToInt(rightCell) + 8 * ConvertBoolToInt(downCell);
+            dungeonGrid[x, y].spriteValue = value;
+        }
+
+        int ConvertBoolToInt(bool value)
+        {
+            return value ? 1 : 0;
+        }
     }
 
-    public class DungeonCell{
+    public class DungeonCell
+    {
         DungeonCellType _cellType;
-        public DungeonCellType cellType{
-            get{
+        DungeonCellFillType _fillType;
+
+        public float pathValue = 1f;
+        public float wallValue = 1f;
+
+        public int spriteValue;
+
+        public DungeonCellType cellType
+        {
+            get
+            {
                 return _cellType;
             }
         }
 
-        public DungeonCell(DungeonCellType type){
+        public DungeonCellFillType fillType
+        {
+            get
+            {
+                return _fillType;
+            }
+        }
+
+        public DungeonCell(DungeonCellType type)
+        {
+            _cellType = type;
+            _fillType = DungeonCellFillType.NON;
+        }
+
+        public void SetCellType(DungeonCellType type)
+        {
             _cellType = type;
         }
 
-        public void SetCellType(DungeonCellType type){
-            _cellType = type;
+        public void SetFillType(DungeonCellFillType type)
+        {
+            _fillType = type;
         }
     }
 }
